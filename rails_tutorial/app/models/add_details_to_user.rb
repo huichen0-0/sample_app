4c7d1541ea9250ac7b517fecdd270d58cf1ec820
huichen0-0 <huyquangnguyeen@gmail.com>
2024-07-03 18:53:27 +0700

    Chapter 1,2: Introduction

---
+++ rails_tutorial/app/models/add_details_to_user.rb
@@ -0,0 +1,2 @@
+class AddDetailsToUser < ApplicationRecord
+end
--- rails_tutorial/db/schema.rb
+++ rails_tutorial/db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_02_142031) do
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "phone"
+    t.string "emails"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
