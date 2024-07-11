836b3fd9a93ee105d7b24fe0b098f5b2d32a6abd
huichen0-0 <huyquangnguyeen@gmail.com>
2024-07-11 15:17:00 +0700

    chapter 6

--- rails_tutorial/app/models/user.rb
+++ rails_tutorial/app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   before_save :downcase_email
 
-  validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true, length: {maximum: 255},
-             format: {with: VALID_EMAIL_REGEX},
+  validates :name, presence: true, 
+             length: {maximum: Settings.user.max_name_length}
+  validates :email, presence: true, 
+             length: {maximum: Settings.user.max_email_length},
+             format: {with: Regexp.new(Settings.user.email_regex)},
              uniqueness: true
 
   has_secure_password
