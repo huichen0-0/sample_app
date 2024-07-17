bb0b173491ab96238a6c27e1ec76bd617fb9b136
huichen0-0 <huyquangnguyeen@gmail.com>
2024-07-17 08:51:52 +0700

    Chapter 9

--- rails_tutorial/app/models/user.rb
+++ rails_tutorial/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save :downcase_email
 
   validates :name, presence: true,
@@ -19,6 +21,25 @@ class User < ApplicationRecord
              end
       BCrypt::Password.create string, cost:
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_token
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
   end
 
   private
