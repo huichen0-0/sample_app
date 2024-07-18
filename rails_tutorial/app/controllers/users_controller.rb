6b8a682be75f8f08050f0b90f5f6c0a4c91ef987
huichen0-0 <huyquangnguyeen@gmail.com>
2024-07-18 17:25:38 +0700

    Chapter 10: Manage user

--- rails_tutorial/app/controllers/users_controller.rb
+++ rails_tutorial/app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i(show)
+  before_action :logged_in_user, except: %i(show new create)
+  before_action :find_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
 
-  # GET /users or /users.json
+  # GET /users or /users.jsons
   def index
-    @users = User.all
+    @pagy, @users = pagy User.desc
   end
 
   # GET /users/1 or /users/1.json
@@ -11,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.find_by id: params[:id]
     return if @user
 
-    flash[:warning] = t ".not_found_user"
+    flash[:warning] = t "warning_not_found"
     redirect_to root_path
   end
 
@@ -33,10 +36,49 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".success"
+    else
+      flash[:danger] = t ".fail"
+    end
+    redirect_to users_path
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_user
+  def find_user
     @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = t "warning_not_found"
+    redirect_to root_path
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "warning_login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    return if current_user? @user
+
+    flash[:error] = t "warning_edit"
+    redirect_to root_url
   end
 
   # Only allow a list of trusted parameters through.
