662cc39d5663b542c08abac38ae42df7c743e05d
huichen0-0 <huyquangnguyeen@gmail.com>
2024-07-25 16:57:41 +0700

    Chapter 13

---
+++ rails_tutorial/app/models/micropost.rb
@@ -0,0 +1,16 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image do |attachable|
+    attachable.variant :display,
+                       resize_to_limit: [Settings.image.width,
+                                         Settings.image.height]
+  end
+
+  validates :user_id, presence: true
+  validates :content, presence: true,
+                      length: {maximum: Settings.micropost.max_content_length}
+  validates :image, content_type: {in: %w(image/jpeg image/gif image/png)},
+                    size: {less_than: Settings.image.size_5.megabytes}
+
+  scope :desc, ->{order created_at: :desc}
+end
